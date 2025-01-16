34411180cd00616ef4a3703a98d79fdbea04a8be
Julia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-01-16 14:12:17 -0300

    Escrever teste

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -2,10 +2,16 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import { VICTORY_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
-  it('renders properly', () => {
-    const wrapper = mount(WordleBoard, { props: { msg: 'Hello World' } })
-    expect(wrapper.text()).toContain('Hello World')
+  test('victory message', async () => {
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' }})
+    const guessInput = wrapper.find("input[type=text]")
+    await guessInput.setValue('TESTS')
+    await guessInput.trigger("keydown.enter")
+
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+  
   })
 })
